2e21502961b719b23834a472edf12e2518ee61dc
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2022-11-26 22:41:22 +0700

    modify package json, smaller build

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -10,18 +10,31 @@ import swaggerUi from '@fastify/swagger-ui'
  * @see https://github.com/fastify/fastify-swagger-ui
  */
 export default fp<FastifyDynamicSwaggerOptions>(async (fastify, opts) => {
+  if (process.env.NODE_ENV === 'production') {
+    return
+  }
+
+  const servers = [
+    {
+      url: `http://${process.env.FASTIFY_ADDRESS}:${process.env.FASTIFY_PORT}`,
+      description: 'Local development server'
+    },
+  ]
+
+  if (process.env.SWAGGER_BASE_URL) {
+    servers.push({
+      url: process.env.SWAGGER_BASE_URL,
+      description: 'Local development server'
+    })
+  }
+
   fastify.register(swagger, {
     openapi: {
       info: {
         title: 'Firdle API',
         version: '0.1.0'
       },
-      servers: [
-        {
-          url: `http://${process.env.FASTIFY_ADDRESS}:${process.env.FASTIFY_PORT}`,
-          description: 'Local development server'
-        },
-      ]
+      servers
     }
   })
 
